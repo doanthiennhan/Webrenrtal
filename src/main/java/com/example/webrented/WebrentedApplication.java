1193f90de72fcb0f6c91b9e4f27251930d50e7d9
Doãn Thiên Nhân <132920167+doanthiennhan@users.noreply.github.com>
2024-05-04 12:05:37 +0700

    Sườn bài

---
+++ src/main/java/com/example/webrented/WebrentedApplication.java
@@ -0,0 +1,13 @@
+package com.example.webrented;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrentedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrentedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webrented/WebrentedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webrented;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrentedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
